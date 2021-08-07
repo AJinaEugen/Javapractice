3a48727467d1af37159dc899af6a1a1cc14008b8
AJinaEugen <alexandru.jina@gmail.com>
2021-08-07 10:44:21 +0300

    EXemple despre mostenire si incapsulare

---
+++ src/test/java/AccessControl/FructeTest.java
@@ -0,0 +1,29 @@
+package AccessControl;
+
+import org.junit.Test;
+
+public class FructeTest {
+
+
+    @Test
+    public void fructtest(){
+
+        System.out.println("Exemple: public ");
+        Fruct obiect= new Fruct("piersica", "Rosie", "rotunda", "5 lei");
+        Banana banana = new Banana("banana","galben","cilindrica", "3 lei");
+
+        obiect.printnumefruct();
+        banana.printnumefruct();
+
+        obiect.printculoare();
+        banana.printculoare();
+
+        obiect.afiseazapret();
+        banana.afiseazapret();
+
+
+
+    }
+
+
+}
